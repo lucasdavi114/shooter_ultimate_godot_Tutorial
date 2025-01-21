281d879f0833e28bb7d2dc0a14f81449608c3d5d
unknown <lucasdaviribeiro46@gmail.com>
2025-01-21 20:47:10 -0300

    Level Interno

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,5 +1,5 @@
 extends LevelParent
 
-func _on_gate_player_entrou_na_area(_body) -> void:
+func _on_saida_body_entered(_body: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property($Player, "velocidade", 0, 1)
